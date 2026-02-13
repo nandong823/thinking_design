85378fe7b52ee5c4d437ad5e0a3830349899d377
ND <nandong823@gmail.com>
2026-02-12 23:34:31 -0500

    Quartz sync: Feb 12, 2026, 11:34 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://nandong823.github.io/thinking_design/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
